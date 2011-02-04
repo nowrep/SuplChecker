d590ac2d9584afaa208e1715bea1e81c5e9437c6
nowrep <nowrep@gmail.com>
2011-02-04 12:58:41 +0100

    mispelling fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -117,7 +117,7 @@ void MainWindow::zacni_loadovat(QString uzjmeno, QString uzheslo, QString server
 void MainWindow::info_o_programu()
 {
     QMessageBox msgBox;
-    msgBox.setText("<h1>SuplChecker 0.2</h1>Jednoduchý checker suplů a známek<br/><br/><b>Autor:</b> davnowrep/><b>Poděkování:</b> Rajnymu a Patrickovi<br/><br/><small>Build time: 06/12/2010 20:57<br/>Copyright (C) 2010-2011 nowrep<a href='http://suplchecker.wz.cz'>http://suplchecker.wz.cz</a>");
+    msgBox.setText("<h1>SuplChecker 0.2</h1>Jednoduchý checker suplů a známek<br/><br/><b>Autor:</b> nowrep/><b>Poděkování:</b> Rajnymu a Patrickovi<br/><br/><small>Build time: 06/12/2010 20:57<br/>Copyright (C) 2010-2011 nowrep<a href='http://suplchecker.wz.cz'>http://suplchecker.wz.cz</a>");
     msgBox.setIcon(QMessageBox::Information);
     msgBox.setWindowIcon(QIcon(":icon.png"));
     msgBox.setIconPixmap(QPixmap(":icon.png"));
